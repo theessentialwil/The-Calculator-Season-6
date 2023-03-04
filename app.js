55b2bd5c62d37c8d7f389f46769af87868ca4a23
mukulembezewil <wilmukulembeze@gmail.com>
2023-03-04 07:41:23 +0300

    cleanded app.js

--- app.js
+++ app.js
@@ -1,25 +1,14 @@
-// DEPENDENCIES (Or Plugins--------***** section in Anatomy of an Express Server
+// DEPENDENCIES (Or Plugins--------*****
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const config = require('./config/db');
-const passport = require('passport');
-
-// Express session
-const expressSession = require('express-session')({
-  secret: 'hermit#9',
-  resave: false,                                        // don't save session if unmodified
-  saveUninitialized: false,                             // don't create session until something is stored
-})
-
-// Import the User Model or Schema
-const ClientRegister = require('./models/entries');
 
 // Importing Route files
 const calcroute = require('./routes/calcroute');
 const { exists } = require('./models/entries');
 
-// INSTANTIATIONS----------------****** section in Anatomy of an Express Server
+// INSTANTIATIONS----------------
 const app = express();
 
 // SetUp Database Connections
@@ -36,7 +25,7 @@ db.on('error', function(err){
   console.error(err);
 });
 
-// CONFIGURATIONS-------------------**** section in Anatomy of an Express Server
+// CONFIGURATIONS-------------------
 app.engine('pug', require('pug').__express);
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
@@ -44,9 +33,8 @@ app.set('views', path.join(__dirname, 'views'));
 // MIDDLEWARE
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(expressSession);
 
-// ROUTES---------------------------**** section in Anatomy of an Express Server
+// ROUTES---------------------------
 app.use('/', calcroute);
 
 app.get('*', (req, res) => {
