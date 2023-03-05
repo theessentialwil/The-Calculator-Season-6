b3eb3081e06b1dfdb0783097644643557da99126
alindaByamukama <alindabyamukama@gmail.com>
2023-03-05 16:55:20 +0300

    add env file to project w/ dummy data - install dotenv - update app js file with dotenv import - test run successful

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 // DEPENDENCIES (Or Plugins--------***** section in Anatomy of an Express Server
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
@@ -8,9 +10,9 @@ const passport = require('passport');
 // Express session
 const expressSession = require('express-session')({
   secret: 'hermit#9',
-  resave: false,                                        // don't save session if unmodified
-  saveUninitialized: false,                             // don't create session until something is stored
-})
+  resave: false, // don't save session if unmodified
+  saveUninitialized: false, // don't create session until something is stored
+});
 
 // Import the User Model or Schema
 const ClientRegister = require('./models/entries');
@@ -23,21 +25,22 @@ const { exists } = require('./models/entries');
 const app = express();
 
 // SetUp Database Connections
-mongoose.connect(config.database, { useNewUrlParser: true});
+mongoose.connect(config.database, { useNewUrlParser: true });
 const db = mongoose.connection;
 
 // Check connection
-db.once('open', function(){
+db.once('open', () => {
   console.log('Connected to MongoDB now');
 });
 
 // Check for db errors
-db.on('error', function(err){
+db.on('error', (err) => {
   console.error(err);
 });
 
 // CONFIGURATIONS-------------------**** section in Anatomy of an Express Server
 app.engine('pug', require('pug').__express);
+
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -50,9 +53,9 @@ app.use(expressSession);
 app.use('/', calcroute);
 
 app.get('*', (req, res) => {
-  res.send("404! Sorry the page you're looking for does not exists.")
-})
+  res.send("404! Sorry the page you're looking for does not exists.");
+});
 
 // BOOTSTRAPPING SERVER--------------***section in Anatomy of an Express Server
 
-app.listen(3800, () => console['log']('Elders are listening on port 3800'));
+app.listen(3800, () => console.log('Elders are listening on port 3800'));
