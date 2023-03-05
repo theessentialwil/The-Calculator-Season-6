f9fbbc4227944f3cb28a499d69fac71f69ef1c5b
alindaByamukama <alindabyamukama@gmail.com>
2023-03-05 19:12:06 +0300

    add changes - comment out previous db connection details

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
-module.exports = {
-  database: "mongodb://localhost:27017/stevoscalculator",
-  secret: "theemperor#4",
-  }; 
+// module.exports = {
+//   database: "mongodb://localhost:27017/stevoscalculator",
+//   secret: "theemperor#4",
+//   };
